56c9b0a8629e66841058b94e448959e3af3d8e71
Alexey <37904920+fliseno1k@users.noreply.github.com>
2024-01-08 07:36:29 +0000

    update factory method signature

--- src/single-modal.ts
+++ src/single-modal.ts
@@ -1,8 +1,11 @@
 import { SingleModalRoot } from './components';
+import { useProtectedApi, usePublicApi } from './hooks';
 import type { SingleModalOptions, SingleModalAPI } from './types';
 
 export function createSingleModal(options: SingleModalOptions): SingleModalAPI {
 	return {
 		Component: SingleModalRoot,
+		usePublicApi,
+		useProtectedApi,
 	} as SingleModalAPI;
 }
