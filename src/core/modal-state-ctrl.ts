a08b0bdf5f80df705f82be741ec2428855b66e65
fliseno1k <fliseno1k@gmail.com>
2024-01-18 00:59:30 +0200

    update modal controller holding state

--- src/core/modal-state-ctrl.ts
+++ src/core/modal-state-ctrl.ts
@@ -1,36 +1,48 @@
-import { atom, action, WritableAtom } from 'nanostores';
+import { atom, action, map } from 'nanostores';
 import { genActionSubscriber } from '../utils/gen-action-subscriber';
-import type { SingleModalView } from '..';
+import type { SingleModalOptions } from '..';
 
 export const enum ModalControllerActions {
 	OPEN = 'OPEN',
 	CLOSE = 'CLOSE',
+	STORE_ENTRY_OPTIONS = 'STORE_ENTRY_OPTIONS',
 }
 
 const $open = atom(false);
+const $entryOptions = map<SingleModalOptions>({} as SingleModalOptions);
 
-const open = action($open, ModalControllerActions.OPEN, ($store, view: SingleModalView['key']) => {
+const open = action($open, ModalControllerActions.OPEN, ($store) => {
 	$store.set(true);
-
 	return true;
 });
 
-const close = action($open, ModalControllerActions.CLOSE, ($store, options: { force?: boolean }) => {
+const close = action($open, ModalControllerActions.CLOSE, ($store) => {
 	$store.set(false);
-
 	return true;
 });
+
+const storeEntryOptions = action(
+	$entryOptions,
+	ModalControllerActions.STORE_ENTRY_OPTIONS,
+	($store, options: SingleModalOptions) => {
+		$store.set(options);
+		return true;
+	},
+);
 
 const actionsMap = {
 	[ModalControllerActions.OPEN]: open,
 	[ModalControllerActions.CLOSE]: close,
+	[ModalControllerActions.STORE_ENTRY_OPTIONS]: storeEntryOptions,
 };
 
 const on = genActionSubscriber($open, actionsMap);
 
 export const ModalController = {
 	$open,
+	$entryOptions,
 	on,
 	open,
 	close,
+	storeEntryOptions,
 };
--- src/single-modal.ts
+++ src/single-modal.ts
@@ -1,12 +1,15 @@
 import { SingleModalRoot } from './components';
 import { useProtectedApi, usePublicApi } from './hooks';
 import { SmError, invariant } from './utils';
+import { ModalController } from './core';
 import type { SingleModalOptions, SingleModalAPI } from './types';
 
 export function createSingleModal<const Options extends SingleModalOptions>(options: Options): SingleModalAPI<Options> {
 	const { views, loader, modal } = options;
 	const isAllRequiredOptionsProvided = views && loader && modal;
 	invariant(isAllRequiredOptionsProvided, SmError.PROVIDE_ALL_REQUIRED_OPTIONS);
+
+	ModalController.storeEntryOptions(options);
 
 	return {
 		Component: SingleModalRoot,
