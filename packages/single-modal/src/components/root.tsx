1932b21bc008be284d6a3fd3bb1c80cd21cd7913
Alexey <37904920+fliseno1k@users.noreply.github.com>
2024-03-12 08:28:30 +0000

    chore: remove extra condition

--- packages/single-modal/src/components/root.tsx
+++ packages/single-modal/src/components/root.tsx
@@ -5,8 +5,6 @@ import { usePrivateApi } from '../hooks';
 export const SingleModalRoot = memo(() => {
 	const { isOpen, view, Inserted, loading } = usePrivateApi();
 
-	if (!Inserted) return;
-
 	return (
 		<SingleModalContext.Provider value={true}>
 			<Inserted isOpen={isOpen} view={view} loading={loading} />
